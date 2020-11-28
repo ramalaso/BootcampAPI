b0ba8c075cd44deeac709b1f6c973e957cb6e721
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-28 19:47:36 -0400

    Update methods Rest API

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ succes: true, message: 'Show all bootcamps.' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ succes: true, message: 'Get bootcamp.' + req.params.id });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(201).json({ succes: true, message: 'Create new bootcamp.' });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ succes: true, message: 'Update a bootcamp.' + req.params.id });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ succes: true, message: 'Delete bootcamp ' + req.params.id });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
